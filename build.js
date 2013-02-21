9fd739c12c20ae10b9c311c67ddcddff10ef4c3d
Patrik Stutz <patrik.stutz@gmail.com>
2013-02-21 10:40:38 -0600

    replaced middleware with build script

--- build.js
+++
@@ -1,7 +0,0 @@
-var less = require("less");
-var fs = require("fs");
-var path = require("path");
-
-less.render(fs.readFileSync(path.resolve(__dirname,"./style/style.less"))+"",function(e,css){
-    fs.writeFileSync(path.resolve(__dirname,"./static/style.css"),css);
-});
